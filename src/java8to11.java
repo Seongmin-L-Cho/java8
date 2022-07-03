595b8c79f352e2fc795a29c63373506a1097012c
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2022-07-03 22:07:31 +0900

    comparator

--- src/java8to11.java
+++ src/java8to11.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Spliterator;
 
@@ -12,19 +13,26 @@ public class java8to11 {
         name.add("ㅋㅌㅊ");
 //        name.forEach(System.out::println); //메서드 레퍼런스 | 이터레이터
 
-        Spliterator<String> spliterator = name.spliterator();
-        Spliterator<String> spliterator1 = spliterator.trySplit(); // 기존 spliterator 반으로 나눈다
+//        Spliterator<String> spliterator = name.spliterator();
+//        Spliterator<String> spliterator1 = spliterator.trySplit(); // 기존 spliterator 반으로 나눈다
 //        ghk
 //        ㅋㅌㅊ
 //         ====
 //        abc
 //        def
         // 결과
-        while(spliterator.tryAdvance(System.out::println));
-        System.out.println("====");
-        while(spliterator1.tryAdvance(System.out::println)); // tryadvance hasnext 비슷한 기능
+//        while(spliterator.tryAdvance(System.out::println));
+//        System.out.println("====");
+//        while(spliterator1.tryAdvance(System.out::println)); // tryadvance hasnext 비슷한 기능
+
+        name.removeIf(s->s.startsWith("a")); // a로 시작하면 지운다
+
+        Comparator<String> comparator = String::compareToIgnoreCase;// 문자순 정렬
+        name.sort(comparator.reversed() ); //comparator.thenComparing() thenComparing 추가 비교 필요
+        // comparator ->
 
 
+        // webMvcConfigure
     }
 
 }
