d9596e3242e51681c82a6f3add3c5a30dbb9dffa
DESKTOP-NJGRTB8\giner <Seongmin.L.Cho@gmail.com>
2022-07-03 20:40:56 +0900

    Spliterator / foreach

--- src/java8to11.java
+++ src/java8to11.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Spliterator;
+
 public class java8to11 {
 
-    RunSomething runSomething = () -> System.out.println("hello");
-    // 람다는 함수형 인터페이스의 구현이라고 봐도 된다.
+    public static void main(String[] args){
+        List<String> name = new ArrayList<>();
+        name.add("abc");
+        name.add("def");
+        name.add("ghk");
+        name.add("ㅋㅌㅊ");
+//        name.forEach(System.out::println); //메서드 레퍼런스 | 이터레이터
+
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit(); // 기존 spliterator 반으로 나눈다
+//        ghk
+//        ㅋㅌㅊ
+//         ====
+//        abc
+//        def
+        // 결과
+        while(spliterator.tryAdvance(System.out::println));
+        System.out.println("====");
+        while(spliterator1.tryAdvance(System.out::println)); // tryadvance hasnext 비슷한 기능
+
+
+    }
+
 }
